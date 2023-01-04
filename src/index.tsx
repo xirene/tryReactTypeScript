58a082be22563960f21286c8e07b1ffa850fd8cf
JY <xirene@naver.com>
2023-01-04 23:51:55 +0900

    tsx Init Setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Router from './Router';
+import './styles/reset.scss';
+import './styles/common.scss';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render();
+root.render(<Router />);
